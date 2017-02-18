52759f83aeb8fa36cd39d2f8f326b5d104e2b448
Danielle Roberson <danielle.roberson@icloud.com>
2017-02-17 22:30:45 -0800

    Frustrated with Sass
    Converted to regular CSS 
    Sassafrassdjhgkdjghks :/

--- js/scripts.js
+++ js/scripts.js
@@ -7,6 +7,11 @@ $('h1').fitText(1.2);
 // Video Player
 $('video').mediaelementplayer();
 
+//FitVids
+$('video').fitVids();
+
+//FitVids
+$('#video').fitVids();
 
 //Background Video
 $('#video').YTPlayer({
