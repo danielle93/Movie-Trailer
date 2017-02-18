ff101e87f68d6097cd105dfbab0a68386a1f8426
Danielle Roberson <danielle.roberson@icloud.com>
2017-02-17 22:43:43 -0800

    Add files via upload

---
+++ Movie-Trailer-master/js/scripts.js
@@ -0,0 +1,20 @@
+// Lettering
+$('h1').lettering();
+
+//FitText
+$('h1').fitText(1.2);
+
+// Video Player
+$('video').mediaelementplayer();
+
+//FitVids
+$('video').fitVids();
+
+//FitVids
+$('#video').fitVids();
+
+//Background Video
+$('#video').YTPlayer({
+    fitToBackground: true,
+    videoId: 'vYsj4NkC4lM'
+});
